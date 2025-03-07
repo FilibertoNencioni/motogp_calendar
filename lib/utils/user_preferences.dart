a44c3d94f599eb2d081104b056b3153d98fe4636
FilibertoNencioni <filiberto.nencioni02@gmail.com>
2025-03-07 20:31:22 +0100

    Getting default system locale

--- lib/utils/user_preferences.dart
+++ lib/utils/user_preferences.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:motogp_calendar/l10n/my_l10n.dart';
 import 'package:motogp_calendar/models/broadcaster.dart';
@@ -24,13 +26,33 @@ class UserPreferences{
   
   ///Return the user locale from his preferences
   static AppLocale getLocale(){
-    String localeCode = _userPref.getString(keyLocale) ?? "en";
-    return appLocales.firstWhere((e)=>e.code == localeCode);
+    String? localeCode = _userPref.getString(keyLocale);
+    //If it was already setted, return the app locale 
+    if(localeCode != null){
+      return appLocales.firstWhere((e)=>e.code == localeCode);
+    }
+
+    //First time the app is opened!
+    //Check if the system locale exists in the app supported ones
+    //and set it as default
+    String sysLocaleName = Platform.localeName;
+    String sysLocaleCode = sysLocaleName.contains("_") ? sysLocaleName.split("_").first : sysLocaleName;
+
+    AppLocale locale = appLocales.firstWhere(
+      (l)=>l.code == sysLocaleCode,
+      orElse: ()=> appLocales.first
+    );
+
+    setLocale(locale.code, null);
+    return locale;
   }
 
-  static void setLocale(String localeCode, BuildContext context) {
+  static void setLocale(String localeCode, BuildContext? context) {
     _userPref.setString(keyLocale, localeCode);
-    MyL10n.of(context)!.changeLocale(Locale(localeCode));
+
+    if(context != null){
+      MyL10n.of(context)!.changeLocale(Locale(localeCode));
+    }
   }
 
   ///Get the user default broadcaster ID 
